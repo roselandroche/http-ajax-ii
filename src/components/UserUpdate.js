6e0446fa36f9cd7477c072fbea2231e240138865
roselandroche <r.landroche@gmail.com>
2019-11-06 20:35:31 -0500

    Following

--- src/components/UserUpdate.js
+++ src/components/UserUpdate.js
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import api from '../utils/api'
 
 function UserUpdate(props) {
+    const [user, setUser] = useState({
+        id: '',
+        name: '',
+        email: ''
+    })
+
+    useEffect(() => {
+        api() 
+            .get(`/users/${props.match.params.id}`)
+            .then(res => {
+                setUser(res.data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }, [props.match.params.id])
+
     return (
         <div>
             <h1>Update User</h1>
